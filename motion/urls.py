926fee56681d98f831997ddaa048149a3e7b3311
Friedrich Kauder <fkauder@gmail.com>
2018-12-28 18:14:38 +0100

    fairly comprehensive functionality

--- motion/urls.py
+++ motion/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from motion.views import LegacyAddView, MotionsView
+from motion.views import LegacyAddView, MotionsView, MotionView
+
 
 urlpatterns = [
     url(r'^actions/add/?$', LegacyAddView.as_view(), name='actions_add'),
+    url(r'^(?P<pk>[0-9]+)/?$', MotionView.as_view(), name='motions'),
     url(r'^$', MotionsView.as_view(), name='motions'),
 ]
--- motion/views.py
+++ motion/views.py
@@ -1,12 +1,18 @@
 import json
 
+from django.http import Http404
 from elasticsearch_dsl import Q
+from elasticsearch_dsl.query import MoreLikeThis
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from motion.documents import MotionDocument
+from motion.models import Motion
 from motion.serializers import MotionSerializer
 from motion.tools import add_motion_from_json
+
+DEFAULT_PAGE = 1
+DEFAULT_PAGE_ITEMS = 30
 
 
 class LegacyAddView(APIView):
@@ -22,27 +28,42 @@ class MotionsView(APIView):
 
     def get(self, request):
         search_options = json.loads(request.GET.get('search'))
+        page = int(request.GET.get('page', DEFAULT_PAGE)) - 1
+        page_items = int(request.GET.get('pageItems', DEFAULT_PAGE_ITEMS))
 
         query = build_search_query(search_options)
 
-        s = MotionDocument.search().query(query)[:30]
+        index_from = page * page_items
+        index_to = index_from + page_items
+
+        sort_options = ['-convention.year', '-convention.slot']
+
+        s = MotionDocument.search().sort(*sort_options).query(query)[index_from:index_to]
         qs = s.to_queryset()
-        return Response(data=MotionSerializer(qs, many=True).data)
+        data = {
+            'page': page,
+            'page_items': page_items,
+            'items': MotionSerializer(qs, many=True).data,
+            'total': s.count()
+        }
+        return Response(data=data)
 
 
 def build_search_query(search_options):
-    if not search_options:
-        return Q()
+    query = None
 
-    query = Q("match", body=search_options.get('body', u'')) | Q("match", body=search_options.get('title', u''))
+    if not search_options or search_options.get('body') == u'':
+        query = Q()
+    else:
+        query = Q("multi_match", query=search_options.get('body', u''), fields=['title', 'body'])
 
     # convention
-    if search_options.get('convention_label'):
-        query &= Q('term', convention__label=search_options.get('convention_label'))
+    if search_options.get('convention'):
+        query &= build_convention_query(search_options.get('convention'))
 
     # section
     if search_options.get('section'):
-        query &= Q('term', section__name=search_options.get('section'))
+        query &= build_section_query(search_options.get('section'))
 
     # submitters
     if search_options.get('submitters'):
@@ -54,9 +75,23 @@ def build_search_query(search_options):
 
     # status
     if search_options.get('status'):
-        query &= Q('term', status=search_options.get('status'))
+        query &= build_status_query(search_options.get('status'))
 
     return query
+
+
+def build_section_query(sections):
+    query_parts = []
+    for section_name in sections:
+        query_parts.append(Q('term', section__name=section_name))
+    return Q('bool', must=query_parts)
+
+
+def build_convention_query(conventions):
+    query_parts = []
+    for convention_label in conventions:
+        query_parts.append(Q('term', convention__label=convention_label))
+    return Q('bool', must=query_parts)
 
 
 def build_submitters_query(submitters):
@@ -75,3 +110,22 @@ def build_referrals_query(submitters):
         nested_query = Q('nested', path="referrals", query=term_query)
         query_parts.append(nested_query)
     return Q('bool', must=query_parts)
+
+
+def build_status_query(allowed_statuus):
+    query_parts = []
+    for status in allowed_statuus:
+        query_parts.append(Q('term', status=status))
+    return Q('bool', should=query_parts, minimum_should_match=1)
+
+
+class MotionView(APIView):
+
+    def get(self, request, pk=None):
+        if pk is None:
+            raise Http404()
+        try:
+            motion = Motion.objects.get(pk=pk)
+        except Motion.DoesNotExist:
+            raise Http404()
+        return Response(data=MotionSerializer(motion).data)
